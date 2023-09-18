f9d96b67cc2d8f9a2e2a5a100530511a402591f6
Daniel <danthevip@gmail.com>
2023-09-18 16:07:31 +0300

    First commit

--- src/main/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplication.java
+++ src/main/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpenseTrackerFafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseTrackerFafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerFafApplication.class, args);
+    }
 
 }
--- src/test/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplicationTests.java
+++ src/test/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ExpenseTrackerFafApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
