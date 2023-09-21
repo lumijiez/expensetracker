6628a3ea207a074003d0691a6422c8075b01ca39
mirrerror <cayote20@mail.ru>
2023-09-21 13:22:59 +0300

    test

--- src/main/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplication.java
+++ src/main/java/com/faf223/expensetrackerfaf/ExpenseTrackerFafApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpenseTrackerFafApplication {
-
+    // TODO: todo
     public static void main(String[] args) {
         SpringApplication.run(ExpenseTrackerFafApplication.class, args);
     }
