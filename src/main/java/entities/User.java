0918a51f7aebfbf8b332bb15785683e783caadbc
mirrerror <cayote20@mail.ru>
2023-09-21 11:33:44 +0300

    Add new entities

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,18 +1,26 @@
 package entities;
 
+import java.util.List;
+
 public class User {
-
     private long id;
-    private String name;
-    private String email;
-    private String login;
-    private String password;
+    private String name, email, login, password;
     private Role role;
+    private List<Expense> expenses;
+    private List<Income> incomes;
 
-    public User(long id, String name) {
+    public User(long id, String name, String email, String login, String password, Role role, List<Expense> expenses, List<Income> incomes) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+        this.expenses = expenses;
+        this.incomes = incomes;
     }
+
+    public User() {}
 
     public long getId() {
         return id;
@@ -28,5 +36,53 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    public List<Income> getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(List<Income> incomes) {
+        this.incomes = incomes;
     }
 }
