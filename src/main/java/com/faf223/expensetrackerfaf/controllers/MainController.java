ccea0d393c2fbaca9cc9c7f009389d041a6ae8dc
mirrerror <cayote20@mail.ru>
2023-09-21 10:21:31 +0300

    Added main controller and user entity

--- src/main/java/com/faf223/expensetrackerfaf/controllers/MainController.java
+++ src/main/java/com/faf223/expensetrackerfaf/controllers/MainController.java
@@ -1,13 +1,21 @@
 package com.faf223.expensetrackerfaf.controllers;
 
+import entities.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class GetHelloWorld {
+public class MainController {
 
     @GetMapping("/")
     public String helloWorld() {
         return "Hello, World!";
     }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id) {
+        return new User(id, "Test");
+    }
+
 }
