c05093a2cfc4b7cc23e584acf0bf7dd8b740886f
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-09-21 14:49:49 +0300

    Add repositories

---
+++ src/main/java/com/faf223/expensetrackerfaf/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.faf223.expensetrackerfaf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
