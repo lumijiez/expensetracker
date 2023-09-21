7232cc2f8a40f3b4bc6dab284cfcdae6cf8e332b
mirrerror <cayote20@mail.ru>
2023-09-21 12:41:37 +0300

    Add more requests

--- src/main/java/com/faf223/expensetrackerfaf/controllers/MainController.java
+++ src/main/java/com/faf223/expensetrackerfaf/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.faf223.expensetrackerfaf.controllers;
 
+import entities.Expense;
+import entities.Income;
+import entities.Role;
 import entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -16,6 +19,48 @@ public class MainController {
     @GetMapping("/users/get/{id}")
     public User getUser(@PathVariable int id) {
         return new User(id, "Test", null, null, null, null, null, null);
+    }
+
+    @PostMapping("/users/set/{id}/name")
+    public String setName(@PathVariable int id,
+                          @RequestParam("name") String name) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/email")
+    public String setEmail(@PathVariable int id,
+                           @RequestParam("email") String email) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/login")
+    public String setLogin(@PathVariable int id,
+                           @RequestParam("login") String login) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/password")
+    public String setPassword(@PathVariable int id,
+                              @RequestParam("password") String password) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/role")
+    public String setRole(@PathVariable int id,
+                          @RequestParam("role") Role role) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/expenses")
+    public String setExpenses(@PathVariable int id,
+                              @RequestParam("expenses") List<Expense> expenses) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
+    }
+
+    @PostMapping("/users/set/{id}/incomes")
+    public String setIncomes(@PathVariable int id,
+                             @RequestParam("incomes")List<Income> incomes) {
+        throw new UnsupportedOperationException("Waiting for the DB.");
     }
 
 }
