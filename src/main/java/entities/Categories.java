86dd4f0c15aa950c41097e23af3c9b60852a4e10
mirrerror <cayote20@mail.ru>
2023-09-21 12:07:20 +0300

    Merge categories into one file, add some new ones

--- src/main/java/entities/Categories.java
+++ src/main/java/entities/Categories.java
@@ -3,7 +3,7 @@ package entities;
 public class Categories {
 
     public enum ExpenseCategory {
-        MISC;
+        CREDIT, BUY, BUSINESS, ENTERTAINMENT, RESTAURANTS_AND_CAFE, COMMUNAL_PAYMENTS, SUPERMARKET, MISC;
 
         public String getEffectiveName() {
             return Categories.getEffectiveName(this.name());
@@ -11,7 +11,7 @@ public class Categories {
     }
 
     public enum IncomeCategory {
-        MISC;
+        P2P, SALARY, GIFT, CREDIT, MISC;
 
         public String getEffectiveName() {
             return Categories.getEffectiveName(this.name());
@@ -19,9 +19,14 @@ public class Categories {
     }
 
     private static String getEffectiveName(String name) {
-        String[] arr = name.split("");
-        StringBuilder result = new StringBuilder(arr[0].toLowerCase());
-        for(int i = 1; i < name.length(); i++) result.append(arr[i]);
+        String[] arr = name.split("_");
+        StringBuilder result = new StringBuilder();
+        for(String entry : arr) {
+            String[] entryArr = entry.split("");
+            StringBuilder builder = new StringBuilder(entryArr[0]);
+            for(int i = 1; i < entry.length(); i++) builder.append(entryArr[i].toLowerCase());
+            result.append(builder).append(" ");
+        }
         return result.toString();
     }
 
