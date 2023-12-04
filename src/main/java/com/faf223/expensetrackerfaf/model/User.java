a84c498073a03722a38b12637185f88b939bc128
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-12-04 15:57:22 +0200

    Automatic google auth fix

--- src/main/java/com/faf223/expensetrackerfaf/model/User.java
+++ src/main/java/com/faf223/expensetrackerfaf/model/User.java
@@ -34,8 +34,6 @@ public class User {
     private String username;
 
     @Transient
-    @NotNull(message = "Password must not be null")
-    @NotEmpty(message = "Password must not be empty")
     private String password;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
