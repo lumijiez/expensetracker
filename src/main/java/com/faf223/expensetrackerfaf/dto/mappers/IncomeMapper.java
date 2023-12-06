2a83718dd6b01a04e6d5d861e583700bb4622ca3
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2023-12-06 17:23:05 +0200

    Lombok optimization

--- src/main/java/com/faf223/expensetrackerfaf/dto/mappers/IncomeMapper.java
+++ src/main/java/com/faf223/expensetrackerfaf/dto/mappers/IncomeMapper.java
@@ -6,24 +6,18 @@ import com.faf223.expensetrackerfaf.model.Expense;
 import com.faf223.expensetrackerfaf.model.Income;
 import com.faf223.expensetrackerfaf.service.IncomeCategoryService;
 import com.faf223.expensetrackerfaf.service.IncomeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
+@RequiredArgsConstructor
 public class IncomeMapper {
 
-    private final IncomeService incomeService;
     private final IncomeCategoryService incomeCategoryService;
     private final UserMapper userMapper;
-
-    @Autowired
-    public IncomeMapper(IncomeService incomeService, IncomeCategoryService incomeCategoryService, UserMapper userMapper) {
-        this.incomeService = incomeService;
-        this.incomeCategoryService = incomeCategoryService;
-        this.userMapper = userMapper;
-    }
 
     public IncomeDTO toDto(Income income) {
         return new IncomeDTO(income.getId(), userMapper.toDto(income.getUser()),
--- src/test/java/com/faf223/expensetrackerfaf/service/IncomeServiceTest.java
+++ src/test/java/com/faf223/expensetrackerfaf/service/IncomeServiceTest.java
@@ -1,23 +1,48 @@
 package com.faf223.expensetrackerfaf.service;
 
+import com.faf223.expensetrackerfaf.model.Income;
+import com.faf223.expensetrackerfaf.model.IncomeCategory;
+import com.faf223.expensetrackerfaf.model.User;
+import com.faf223.expensetrackerfaf.repository.IncomeCategoryRepository;
 import com.faf223.expensetrackerfaf.repository.IncomeRepository;
+import com.faf223.expensetrackerfaf.repository.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 public class IncomeServiceTest {
 
     @Mock
     private IncomeRepository incomeRepository;
-
+    @Mock
+    private IncomeCategoryRepository incomeCategoryRepository;
+    @Mock
+    private UserRepository userRepository;
     @InjectMocks
     private IncomeService incomeService;
 
     @Test
     public void IncomeService_CreateIncome_ReturnsIncome() {
+
+        // TODO: move income creation to service or move this test to controller tests
+        Optional<User> user = userRepository.findByUsername("Deep Deep");
+        Assertions.assertThat(user.isPresent()).isTrue();
+        IncomeCategory incomeCategory = incomeCategoryRepository.getReferenceById(4L);
+
+        Income income = Income.builder()
+                .user(user.get())
+                .amount(BigDecimal.valueOf(700))
+                .category(incomeCategory)
+                .date(LocalDate.of(2023,10,5))
+                .build();
 
 
 
