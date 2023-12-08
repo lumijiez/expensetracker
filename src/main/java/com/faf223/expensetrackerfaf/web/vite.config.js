b22f3c32d0e76d1bdeb4d35e3f62b413d3aa09e7
Daniel <danthevip@gmail.com>
2023-12-09 00:38:15 +0200

    Front upd

--- src/main/java/com/faf223/expensetrackerfaf/web/vite.config.js
+++ src/main/java/com/faf223/expensetrackerfaf/web/vite.config.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-// ffddddd
+// ffdddddd
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
